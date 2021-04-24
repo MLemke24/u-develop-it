c9ecaa9183dfebd4d69442cf17404fa221cd0cc1
mlemke24 <mattrlemke@gmail.com>
2021-04-24 13:42:32 -0500

    database connection added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 
@@ -6,9 +7,20 @@ const app = express()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Lemke1020',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 app.use((req, res) => {
     res.status(404).end();
